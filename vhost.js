8547089fd053e0405071c9883369f1cdc9ccdd59
GuillianV <guillianvibert90@gmail.com>
2022-09-20 20:33:49 +0200

    add vhost

---
+++ vhost.js
@@ -0,0 +1,25 @@
+require('../../server.js')
+
+module.exports = function (app, express, vhost, PrepareBasicRoutes) {
+
+    const cdn = [
+        'www.fleme.fr',
+        'fleme.fr',
+    ]
+
+
+    let flemeApp = require("./app.js")
+
+    
+    app.use(vhost(cdn, flemeApp))
+
+
+    
+
+    let cert = {
+        key: 'key.pem',
+        cert: 'cert.pem'
+    }
+    return [cdn, cert]
+
+};
