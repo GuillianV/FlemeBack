d4a76d4d5e5a576d76e762e59a54c83ff4534735
lsmnt <leasimonet56@gmail.com>
2022-09-21 13:51:54 +0200

    add exported text

--- routes/index.js
+++ routes/index.js
@@ -28,7 +28,7 @@ router.get('/:recognizeUrl',  async function(req, res, next) {
     let text = ""
     for (let line of recognize.text.split("\n")) {
       text += line.replace(/"/g,"'") + "/!:/";
-	
+
     }
     res.render('recognize', { text: text });
 
